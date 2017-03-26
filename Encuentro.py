1ea83edcb1ef7effe49cc23adfb540a4abcc6469
Diego van Haaster <diegomvh@gmail.com>
2017-03-26 08:25:46 -0300

    Mejores nombres

--- Encuentro.py
+++ Encuentro.py
@@ -3,12 +3,11 @@ class Encuentro(set):
     def __init__(self, robot_1, robot_2):
         self.robot_1 = robot_1
         self.robot_2 = robot_2
-        self.matchs = []
+        self.ganadas = []
 
-
-    def raund(self, _robot):
-        self.matchs.append(_robot)
-
+    def gano(self, robot):
+        assert robot is self.robot_1 or robot is self.robot_2, "El robot ganador no es parte del encuentro"
+        self.ganadas.append(robot)
 
     def es_valido(self):
         """Para que el encuentro sea valido los robots deben ser distintos"""
@@ -22,26 +21,24 @@ class Encuentro(set):
         return (self.robot_1 == other.robot_1 and self.robot_2 == other.robot_2) or \
             (self.robot_1 == other.robot_2 and self.robot_2 == other.robot_1)
 
-
     def ganador(self):
-        r1 = [ r for r in self.matchs if r == self.robot_1 ]
-        r2 = [ r for r in self.matchs if r == self.robot_2 ] 
+        r1 = [ r for r in self.ganadas if r == self.robot_1 ]
+        r2 = [ r for r in self.ganadas if r == self.robot_2 ] 
         if len(r1) > len(r2):
             return self.robot_1
         elif len(r1) < len(r2):
             return self.robot_2
-
 
     def __str__(self):
         return "<%s vs %s>" % (self.robot_1, self.robot_2)
 
 def main():
     e = Encuentro("1", "2")
-    e.raund("1")
-    e.raund("2")
-    e.raund("2")
-    e.raund("1")
-    e.raund("1")
+    e.gano("1")
+    e.gano("2")
+    e.gano("2")
+    e.gano("1")
+    e.gano("2")
     print(e.ganador())
 
 if __name__ == '__main__':
