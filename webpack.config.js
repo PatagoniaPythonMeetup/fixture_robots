a3011d9294f9ccfb903ef23c35c70e5c743665a3
Diego van Haaster <diegomvh@gmail.com>
2017-04-03 12:19:43 -0300

    Test webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var entries = ['./client/index'];
+
+module.exports = {
+  entry: entries,
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'client')
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
+  resolve: {
+    alias: {
+      "jquery-bracket": "jquery-bracket/dist/jquery.bracket.min.js",
+      "jquery-bracket-css": "jquery-bracket/dist/jquery.bracket.min.css",
+    }
+  }
+};
