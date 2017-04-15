233f8bb16aa9993fda1627849c6ff4dc872f9daa
Diego van Haaster <diegomvh@gmail.com>
2017-04-15 17:09:08 -0300

    test

--- client/lokkatest.js
+++ client/lokkatest.js
@@ -9,35 +9,52 @@ const client = new Lokka({
   transport: new Transport(`http://${location.host}/fixture`)
 });
 
-client.query(`
-    {
-      robots {
-        nombre
-        escuela
-        encargado
-        score
-      }
-      rondas {
-        numero
-        vuelta
-        jugadas
-        encuentros {
-          numero
-          jugadas
-          robots {
-            nombre
-          }
-        }
-        promovidos {
+let consultarRobots = () =>
+  client.query(`
+      {
+        robots {
           nombre
           escuela
           encargado
+          score
         }
       }
-    }
-`).then(result => {
-    var robots = result.robots;
-    var rondas = result.rondas;
-    console.log(robots)
-});
+  `).then(result => result.robots);
 
+let ganaRobot = (ronda, encuentro, robot) =>
+  client.mutate(`{
+    success: ganaRobot(ronda: ${ronda}, encuentro: ${encuentro}, robot: "${robot}") {
+        ok
+    }
+  }`).then(result => result.success);
+
+ganaRobot(1, 1, "HAL 9000");
+ganaRobot(1, 1, "HAL 9000");
+ganaRobot(1, 1, "Centinelas");
+ganaRobot(1, 2, "ED 209");
+ganaRobot(1, 2, "ED 209");
+ganaRobot(1, 2, "Roy Batty");
+ganaRobot(1, 3, "Ultron");
+ganaRobot(1, 3, "Bender");
+ganaRobot(1, 3, "Bender");
+ganaRobot(1, 4, "Teddy");
+ganaRobot(1, 4, "Wall-e");
+ganaRobot(1, 4, "Teddy");
+ganaRobot(1, 5, "Optimus Prime");
+ganaRobot(1, 5, "Johnny 5");
+ganaRobot(1, 5, "Optimus Prime");
+ganaRobot(1, 6, "Rodney");
+ganaRobot(1, 6, "Rodney");
+ganaRobot(1, 6, "Rodney");
+ganaRobot(1, 7, "BB-8");
+ganaRobot(1, 7, "Robocop");
+ganaRobot(1, 7, "BB-8");
+ganaRobot(1, 8, "T-1000");
+ganaRobot(1, 8, "3-CPO");
+ganaRobot(1, 8, "T-1000");
+ganaRobot(1, 9, "R2-D2");
+ganaRobot(1, 9, "Sony");
+ganaRobot(1, 9, "Sony");
+ganaRobot(1, 10, "T-800");
+ganaRobot(1, 10, "T-800");
+ganaRobot(1, 10, "David Swinton");
