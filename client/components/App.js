9f1dbbf728088c32f76cf69a68706829c5e6deea
Diego van Haaster <diegomvh@gmail.com>
2017-04-24 19:26:51 -0300

    Agrego semantic ui

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Button, Container, Header } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
-    return (<div>
-      { this.props.children }
-    </div>);
+    return (<Container>
+      <Header as='h1'>Hola Mundo</Header>
+      <div>
+        { this.props.children }
+      </div>
+    </Container>);
   }
 }
 
