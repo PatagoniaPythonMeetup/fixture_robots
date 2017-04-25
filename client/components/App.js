8e7d6cf7eaee9eb0b272d7f2ca6cd98c71545a2d
Diego van Haaster <diegomvh@gmail.com>
2017-04-24 22:21:34 -0300

    Probando componentes de semantic ui react

--- client/components/App.js
+++ client/components/App.js
@@ -4,10 +4,7 @@ import { Button, Container, Header } from 'semantic-ui-react'
 class App extends Component {
   render() {
     return (<Container>
-      <Header as='h1'>Hola Mundo</Header>
-      <div>
         { this.props.children }
-      </div>
     </Container>);
   }
 }
--- client/components/Main.js
+++ client/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Router } from 'react-router'
+import { Menu } from 'semantic-ui-react'
 import createBrowserHistory from 'history/createBrowserHistory'
 
 import { routes } from './routes';
@@ -11,11 +12,22 @@ import { Provider } from 'react-redux';
 const history = createBrowserHistory()
 
 class Main extends Component {
+  state = {}
+  
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     return (
       <Provider store={ this.props.store }>
         <Router history={history}>
-          { routes }
+          <Menu>
+            <Menu.Item header>Hackathon</Menu.Item>
+            <Menu.Item name='aboutUs' active={activeItem === 'aboutUs'} onClick={this.handleItemClick} />
+            <Menu.Item name='jobs' active={activeItem === 'jobs'} onClick={this.handleItemClick} />
+            <Menu.Item name='locations' active={activeItem === 'locations'} onClick={this.handleItemClick} />
+            { routes }
+          </Menu>
         </Router>
       </Provider>
     );
