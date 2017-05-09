4c26bab9ba0c56664481755c03118fb0f5c42f6e
Diego van Haaster <diegomvh@gmail.com>
2017-05-09 10:02:21 -0300

    Esquema

--- webclient/src/graphql/schema.ts
+++ webclient/src/graphql/schema.ts
@@ -1,6 +1,43 @@
 //  This file was automatically generated and should not be edited.
 /* tslint:disable */
 
+export interface GanaRobotMutationVariables {
+  key: string;
+  ronda: number | null;
+  encuentro: number | null;
+}
+
+export interface GanaRobotMutation {
+  ganaRobot: {
+    ok: boolean | null,
+    mensaje: string | null,
+    encuentro: {
+      numero: number | null,
+      jugadas: number | null,
+      finalizado: boolean | null,
+      puntos: Array< number | null > | null,
+    } | null,
+  } | null;
+}
+
+export interface GenerarRondaMutationVariables {
+  tct: boolean;
+}
+
+export interface GenerarRondaMutation {
+  generarRonda: {
+    ok: boolean | null,
+    mensaje: string | null,
+    ronda: {
+      numero: number | null,
+      vuelta: number | null,
+      jugadas: number | null,
+      finalizada: boolean | null,
+      tct: boolean | null,
+    } | null,
+  } | null;
+}
+
 export interface RobotsQuery {
   fixture: {
     robots: Array< {
