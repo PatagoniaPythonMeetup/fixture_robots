f57671fd5b6c217828c0a6b05cb0aa68411e04f9
Diego van Haaster <diegomvh@gmail.com>
2017-05-15 13:14:42 -0300

    Por defecto vamos con los robots

--- webclient/src/app/layout/menu/menu.component.ts
+++ webclient/src/app/layout/menu/menu.component.ts
@@ -1,24 +1,30 @@
 import { RondasQuery } from '../../../graphql/schema';
 import { ApolloQueryObservable } from 'apollo-angular/build/src';
 import { FixtureService } from '../../fixture.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
   rondas$: ApolloQueryObservable<any>;
   title = 'Rob Fixture';
-  
+  activo: String;
+
   constructor(private fixture: FixtureService) { }
 
   ngOnInit() {
     this.rondas$ = this.fixture.rondas();
   }
 
-  activar(item, ronda) {
+  public ngAfterViewInit() {
+    this.activar('robots');
+  }
+
+  activar(item, ronda = null) {
+    this.activo = item;
     console.log(item, ronda)
   }
 
