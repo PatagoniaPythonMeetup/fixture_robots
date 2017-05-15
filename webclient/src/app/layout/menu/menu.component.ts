9cdd199053eaeead159bcbafd38e0ea22f81a3ed
Diego van Haaster <diegomvh@gmail.com>
2017-05-15 13:01:27 -0300

    activar items del menu

--- webclient/src/app/layout/menu/menu.component.ts
+++ webclient/src/app/layout/menu/menu.component.ts
@@ -18,6 +18,10 @@ export class MenuComponent implements OnInit {
     this.rondas$ = this.fixture.rondas();
   }
 
+  activar(item, ronda) {
+    console.log(item, ronda)
+  }
+
   generarRonda() {
     this.fixture.generarRonda(false)
       .subscribe(ronda => {
