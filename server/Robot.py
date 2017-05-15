da4df7d240fac36244cfc7ce4e62d889cc7db8b4
Diego van Haaster <diegomvh@gmail.com>
2017-05-15 12:03:12 -0300

    Solo un escudo por robot

--- server/Robot.py
+++ server/Robot.py
@@ -2,16 +2,18 @@ from collections import namedtuple
 import hashlib
 import random
 
+# FIXME: Solo tenemos por el momento 23 imagenes
 IMGS = ["e{0:02d}.png".format(numero) for numero in range(1, 23)]
 
-class Robot(namedtuple("Robot", "nombre escuela encargado")):
+class Robot(namedtuple("Robot", "nombre escuela encargado escudo")):
     __slots__ = ()
     
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 3:
+            args = args + (IMGS.pop(random.randint(0, len(IMGS) - 1)), )
+        return super().__new__(cls, *args, **kwargs)
+
     @property
     def key(self):
         _key = self.nombre + self.escuela + self.encargado
         return hashlib.md5(_key.encode("utf-8")).hexdigest()
-    
-    @property
-    def escudo(self):
-        return random.choice(IMGS)
