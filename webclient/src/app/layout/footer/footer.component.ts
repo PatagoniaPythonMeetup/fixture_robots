6e164d4a3b8f9a40d213c3811d84f9489c13e4ed
Diego van Haaster <diegomvh@gmail.com>
2017-05-15 21:25:32 -0300

    Buscando forma de detectar estado

--- webclient/src/app/layout/footer/footer.component.ts
+++ webclient/src/app/layout/footer/footer.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { FixtureService } from '../../fixture.service';
+import { Component, OnInit, AfterViewChecked, AfterViewInit, AfterContentChecked, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, DoCheck {
   enCurso: Boolean = false
-  constructor() { }
+  constructor(private fixture: FixtureService) { }
 
   ngOnInit() {
+    
   }
 
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  
 }
