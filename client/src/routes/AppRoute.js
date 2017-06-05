dffbb61e47f0a50a7d632c2afd022b954c3758d7
Diego van Haaster <diegomvh@gmail.com>
2017-06-05 20:08:52 -0300

    Quitando cliente react

--- client/src/routes/AppRoute.js
+++
@@ -1,8 +0,0 @@
-import Relay from 'react-relay';
-
-export default class extends Relay.Route {
-  static queries = {
-    fixture: (Component) => Relay.QL`query FixtureQuery { fixture { ${Component.getFragment('fixture')} } }`,
-  };
-  static routeName = 'AppRoute';
-}
